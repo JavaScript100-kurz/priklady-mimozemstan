be56c691912c031980aeafcc464cccf00c8d5e2d
Luděk Roleček <ludek@rolecek.cz>
2023-08-15 19:19:01 +0200

    Reseni

--- script.js
+++ script.js
@@ -13,3 +13,77 @@ const jeSrazka = (obj1, obj2) => {
 	);
 }
 
+const nastavPozici = (obj) => {
+	obj.element.style.left = obj.x + 'px';
+	obj.element.style.top = obj.y + 'px';
+}
+
+
+const priStiskuKlavesy = (event) => {
+
+	if (event.code === 'ArrowRight') {
+		panacek.x += 10;
+		if (panacek.x + panacek.sirka > window.innerWidth) {
+			panacek.x = window.innerWidth - panacek.sirka;
+		}
+		panacek.element.src = 'obrazky/panacek-vpravo.png';
+	}
+
+	if (event.code === 'ArrowLeft') {
+		panacek.x -= 10;
+		if (panacek.x < 0) {
+			panacek.x = 0;
+		}
+		panacek.element.src = 'obrazky/panacek-vlevo.png';
+	}
+
+	if (event.code === 'ArrowUp') {
+		panacek.y -= 10;
+		if (panacek.y < 0) {
+			panacek.y = 0;
+		}
+		panacek.element.src = 'obrazky/panacek-nahoru.png';
+	}
+
+	if (event.code === 'ArrowDown') {
+		panacek.y += 10;
+		if (panacek.y + panacek.vyska > window.innerHeight) {
+			panacek.y = window.innerHeight - panacek.vyska;
+		}
+		panacek.element.src = 'obrazky/panacek.png';
+	}
+
+	nastavPozici(panacek);
+
+	if (jeSrazka(panacek, mince)) {
+		mince.x = 500;
+		mince.y = 100;
+		nastavPozici(mince);
+	}
+}
+
+
+const panacek = {
+	x: 25,
+	y: 100,
+	sirka: 64,
+	vyska: 70,
+	element: document.querySelector('#panacek'),
+}
+
+const mince = {
+	x: 150,
+	y: 210,
+	sirka: 36,
+	vyska: 36,
+	element: document.querySelector('#mince'),
+}
+
+nastavPozici(panacek);
+nastavPozici(mince);
+document.addEventListener('keydown', priStiskuKlavesy);
+
+// nahodna pozice mince pri sebrani
+// pocitat a zobrazovat skore
+// prehravat zvuky
+// ukoncit hru a oznamit vitezstvi pri sebrani 5 minci - alert()
